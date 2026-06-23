02604bf849288141aa97a3b5a968e95d45408000
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-06-23 14:48:52 -0300

    Configure package for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "unplugin-dts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: ["src/main.ts"],
+      entry: resolve(__dirname, "src/index.ts"),
       name: "dlidlib",
       fileName: "dlidlib",
     },
   },
+  plugins: [
+    dts({
+      tsconfigPath: "./tsconfig.lib.json",
+    }),
+  ],
 });
