2dc2b7304708b59d88f38421f78148b209bb6b54
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-06-23 20:05:46 -0300

    Fix missing type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "dlidlib",
-      fileName: "dlidlib",
+      fileName: "index",
     },
   },
   plugins: [
