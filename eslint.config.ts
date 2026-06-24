73f49a0d017b661f6f3566c6bb989e3c35c9527e
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-06-24 11:09:37 -0300

    Reconfigure build process

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,5 @@
 import js from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -10,22 +7,9 @@ export default defineConfig([
     files: ["src/**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   {
     files: ["src/**/*.ts"],
     extends: [tseslint.configs.recommended],
-  },
-  {
-    files: ["src/**/*.json"],
-    plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.md"],
-    plugins: { markdown },
-    language: "markdown/commonmark",
-    extends: ["markdown/recommended"],
   },
 ]);
