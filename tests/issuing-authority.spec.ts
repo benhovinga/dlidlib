e30e3c48443d37979ee149687c8b65c7c87f54d5
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-06-27 21:44:22 -0300

    Change country to 3 character code

--- tests/issuing-authority.spec.ts
+++ tests/issuing-authority.spec.ts
@@ -6,22 +6,20 @@ describe("Test the AUTHORITIES static property", () => {
   test("matches snapshot", () => {
     expect(IssuingAuthority.AUTHORITIES).toMatchSnapshot();
   });
-  test("expect frozen authorities", () => {
+  test("is frozen", () => {
     expect(Object.isFrozen(IssuingAuthority.AUTHORITIES)).toBe(true);
   });
-  test("expect frozen authority", () => {
+  test("item is frozen", () => {
     expect(Object.isFrozen(IssuingAuthority.AUTHORITIES[0])).toBe(true);
   });
 });
 
 describe("Test the getAuthorityById() static method", () => {
-  test("can return a valid authority", () => {
-    expect(IssuingAuthority.getAuthorityById(604426)).toEqual({
-      abbr: "PE",
-      country: "Canada",
-      issuerId: 604426,
-      jurisdiction: "Prince Edward Island",
-    });
+  const issuerIds = IssuingAuthority.AUTHORITIES.map(
+    (authority) => authority.issuerId,
+  );
+  test.each(issuerIds)("matches snapshot for issuerId '%i'", (issuerId) => {
+    expect(IssuingAuthority.getAuthorityById(issuerId)).toMatchSnapshot();
   });
   test("can return undefined when issuer id was not found", () => {
     expect(IssuingAuthority.getAuthorityById(0)).toBe(undefined);
