1194fd263e50d39063c142bca6bf381edeca2081
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-06-30 09:58:03 -0300

    Improve test environment

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: { include: ["src"] },
+    typecheck: {
+      enabled: true,
+      checker: "tsc",
+      tsconfig: "./tsconfig.test.json",
+    },
+  },
+});
